e011f5341f36c9b8299d7a796eb15db06ced92a0
leox01code <leox01code@gmail.com>
2025-07-23 09:29:43 -0600

    vite configs added to handle chunks

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // aumenta el límite a 1000 kB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) return 'firebase';
+            if (id.includes('@mui')) return 'mui';
+            if (id.includes('react')) return 'react';
+          }
+        },
+      },
+    },
+  },
 })
